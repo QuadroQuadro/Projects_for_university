8b4eb20d8cad27fd31e35fab84d980e6a644ab5f
QuadroQuadro <Danil.siwachev@Ya.ru>
2017-05-29 04:21:43 +0300

    Update ClientTCP.cs

--- ClientTCP.cs
+++ ClientTCP.cs
@@ -25,19 +25,15 @@ namespace ClientTCP
 
         static void SendMessageFromSocket(int port)
         {
-            // Буфер для входящих данных
+            
             byte[] bytes = new byte[1024];
 
-            // Соединяемся с удаленным устройством
-
-            // Устанавливаем удаленную точку для сокета
             IPHostEntry ipHost = Dns.GetHostEntry("192.168.1.64");
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
 
             Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-            // Соединяем сокет с удаленной точкой
             sender.Connect(ipEndPoint);
 
             Console.Write("Введите сообщение: ");
@@ -46,19 +42,15 @@ namespace ClientTCP
             Console.WriteLine("Сокет соединяется с {0} ", sender.RemoteEndPoint.ToString());
             byte[] msg = Encoding.UTF8.GetBytes(message);
 
-            // Отправляем данные через сокет
             int bytesSent = sender.Send(msg);
 
-            // Получаем ответ от сервера
             int bytesRec = sender.Receive(bytes);
 
             Console.WriteLine("\nОтвет от сервера: {0}\n\n", Encoding.UTF8.GetString(bytes, 0, bytesRec));
 
-            // Используем рекурсию для неоднократного вызова SendMessageFromSocket()
             if (message.IndexOf("<TheEnd>") == -1)
                 SendMessageFromSocket(port);
 
-            // Освобождаем сокет
             sender.Shutdown(SocketShutdown.Both);
             sender.Close();
         }
